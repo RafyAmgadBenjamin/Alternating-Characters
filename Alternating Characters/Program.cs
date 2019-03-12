1534b5e172e9b2a7a674f2fd14ccbc6a941ef5cb
Rafy <Rafy@DESKTOP-ADC4ST3>
2019-03-12 14:47:44 +0200

    After getting accepted

--- Alternating Characters/Program.cs
+++ Alternating Characters/Program.cs
@@ -8,8 +8,44 @@ namespace Alternating_Characters
 {
     class Program
     {
+        static int alternatingCharacters(string s)
+        {
+            int deletion = 0;
+            for (int i = 0; i < s.Length-1; i++)
+            {
+
+                if(s[i]=='A'&& s[i+1] == 'A')
+                {
+                    deletion++;
+                }
+                else if (s[i] == 'B' && s[i + 1] == 'B')
+                {
+                    deletion++;
+                }
+
+            }
+            return deletion;
+
+        }
+
         static void Main(string[] args)
         {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int q = Convert.ToInt32(Console.ReadLine());
+
+            for (int qItr = 0; qItr < q; qItr++)
+            {
+                string s = Console.ReadLine();
+
+                int result = alternatingCharacters(s);
+
+                Console.WriteLine(result);
+            }
+
+            //textWriter.Flush();
+           // textWriter.Close();
         }
     }
 }
+
